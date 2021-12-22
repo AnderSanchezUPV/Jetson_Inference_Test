43d598c6ad347cc27ea9706c0be40fe1581d3036
AnderSanchezUPV <ander.sanchez@ehu.eus>
2021-12-22 15:02:21 +0100

    Arucos 6x6 50

--- Generar_Arucos.py
+++ Generar_Arucos.py
@@ -5,12 +5,12 @@ import os
 import numpy as np
 from PIL import Image
 
-aruco_dict=cv2.aruco.Dictionary_get(cv2.aruco.DICT_7X7_50)
+aruco_dict=cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_50)
 Aruco_folder_path="images\ArucoFolder"
 
 
-for i in range(50):
+for i in range(51):
     tag = np.zeros((300, 300, 1), dtype="uint8") ## Array de salida
     cv2.aruco.drawMarker(aruco_dict, i, 300, tag, 1)
-    aruco_name=Aruco_folder_path + "\Aruco_id_"+ str(i+1) + ".jpg"
+    aruco_name=Aruco_folder_path + "\Aruco_id_"+ str(i) + ".jpg"
     cv2.imwrite(aruco_name, tag)
